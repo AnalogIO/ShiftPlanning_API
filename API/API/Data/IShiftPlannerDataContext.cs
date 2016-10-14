d5ae93cecfdc8b8854ed50b48c62c127c3125d9e
xthedeus <fhov@itu.dk>
2016-10-14 15:26:38 +0200

    Added context, migration config, user entity and path to secret files: connections.config and appSettings.config.

---
+++ API/API/Data/IShiftPlannerDataContext.cs
@@ -0,0 +1,11 @@
+﻿using API.Models;
+using System;
+using System.Data.Entity;
+
+namespace API.Data
+{
+    public interface IShiftPlannerDataContext : IDisposable
+    {
+        DbSet<User> Users { get; set; }
+    }
+}
---
+++ API/API/Data/ShiftPlannerDataContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.Entity;
+using API.Models;
+
+namespace API.Data
+{
+    public class ShiftPlannerDataContext : DbContext, IShiftPlannerDataContext
+    {
+        public ShiftPlannerDataContext(): base("name=ShiftPlannerDataContext") {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
