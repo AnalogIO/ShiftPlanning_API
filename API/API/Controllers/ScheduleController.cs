3b034bacc55dd5b4cc53886cdb0d34814f7f10b6
xthedeus <fhov@itu.dk>
2016-10-31 15:01:07 +0100

    Added method to retrieve all schedules to schedule controller

--- API/API/Controllers/ScheduleController.cs
+++ API/API/Controllers/ScheduleController.cs
@@ -1,4 +1,6 @@
 ﻿using API.Data;
+using API.Logic;
+using API.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,11 +16,36 @@ namespace API.Controllers
     public class ScheduleController : ApiController
     {
         private IScheduleRepository _scheduleRepository;
+        private IManagerRepository _managerRepository;
 
         public ScheduleController()
         {
             var context = new ShiftPlannerDataContext();
             _scheduleRepository = new ScheduleRepository(context);
+            _managerRepository = new ManagerRepository(context);
+        }
+
+        // GET api/schedules
+        /// <summary>
+        /// Gets all the schedules.
+        /// </summary>
+        /// <returns>
+        /// Returns an array of schedules.
+        /// </returns>
+        [HttpGet, AdminFilter, Route("")]
+        public IHttpActionResult Get()
+        {
+            var manager = GetManager();
+            if (manager == null) return BadRequest("Provided token is invalid!");
+
+            var schedules = _scheduleRepository.ReadFromInstitution(manager.Institution.Id);
+            return Ok(schedules);
+        }
+
+        private Manager GetManager()
+        {
+            var token = Request.Headers.Authorization.ToString();
+            return _managerRepository.Read(token);
         }
 
     }
