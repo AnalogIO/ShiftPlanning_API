ab60107e42eeeff0a3c9a67ec955890bc6b03b60
xthedeus <fhov@itu.dk>
2016-11-01 17:11:17 +0100

    Added method to get a single schedule from a given id. Added method to create a schedule with minimum details such as name and number of weeks - added corresponding dto.

--- API/API/Controllers/ScheduleController.cs
+++ API/API/Controllers/ScheduleController.cs
@@ -1,9 +1,12 @@
 ﻿using API.Data;
 using API.Logic;
 using API.Models;
+using API.Models.DTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
 using System.Web.Http;
 
@@ -40,6 +43,64 @@ namespace API.Controllers
 
             var schedules = _scheduleRepository.ReadFromInstitution(manager.Institution.Id); // proper dto should be used here
             return Ok(schedules);
+        }
+
+        // GET api/schedules/{id}
+        /// <summary>
+        /// Gets the schedule with the given id.
+        /// </summary>
+        /// <param name="id">The id of the schedule.</param>
+        /// <returns>
+        /// Returns the schedule with the given id. 
+        /// If no schedule is found with the corresponding id, the controller will return NotFound (404)
+        /// </returns>
+        [HttpGet, AdminFilter, Route("{id}")]
+        public IHttpActionResult Get(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var manager = GetManager();
+            if (manager == null) return BadRequest("Provided token is invalid!");
+
+            var schedule = _scheduleRepository.Read(id, manager.Institution.Id);
+            if (schedule != null)
+            {
+                return Ok(schedule);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        // POST api/schedules
+        /// <summary>
+        /// Creates the schedule from the content in the body.
+        /// </summary>
+        /// <returns>
+        /// Returns 'Created' (201) if the schedule gets created.
+        /// </returns>
+        [HttpPost, AdminFilter, Route("")]
+        public IHttpActionResult Register(CreateScheduleDTO scheduleDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var manager = GetManager();
+            if (manager == null) return BadRequest("Provided token is invalid!");
+
+            var schedule = new Schedule { Name = scheduleDto.Name, NumberOfWeeks = scheduleDto.NumberOfWeeks };
+            schedule = _scheduleRepository.Create(schedule);
+            if (schedule != null)
+            {
+                return ResponseMessage(new HttpResponseMessage(HttpStatusCode.Created));
+            }
+            return BadRequest("A user with the given email does already exist!");
         }
 
         private Manager GetManager()
---
+++ API/API/Models/DTO/CreateScheduleDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models.DTO
+{
+    public class CreateScheduleDTO
+    {
+        public string Name { get; set; }
+        public int NumberOfWeeks { get; set; }
+    }
+}
