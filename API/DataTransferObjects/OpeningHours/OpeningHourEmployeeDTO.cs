a96aac0b233488be0c32a70ce18e375eb8c6a2de
Mikael Lindemann Jepsen <mikaellindemannjepsen@gmail.com>
2016-11-07 16:12:55 +0100

    Added DTO's for opening hours/public shifts.

---
+++ API/DataTransferObjects/OpeningHours/OpeningHourEmployeeDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace DataTransferObjects.OpeningHours
+{
+    public class OpeningHourEmployeeDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+    }
+}
---
+++ API/DataTransferObjects/OpeningHours/OpeningHoursDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataTransferObjects.OpeningHours
+{
+    public class OpeningHoursDTO
+    {
+        public int Id { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public IEnumerable<OpeningHourEmployeeDTO> Employees { get; set; }
+        public IEnumerable<OpeningHourEmployeeDTO> CheckedInEmployees { get; set; }
+    }
+}
