d57fe7e086b2cfdf53eeee2893c74a5d8d6dfe5c
xthedeus <fhov@itu.dk>
2016-11-14 20:43:17 +0100

    Doc fixes.

--- API/DataTransferObjects/Employee/UpdateEmployeeDTO.cs
+++ API/DataTransferObjects/Employee/UpdateEmployeeDTO.cs
@@ -5,25 +5,25 @@ namespace DataTransferObjects.Employee
     public class UpdateEmployeeDTO
     {
         /// <summary>
-        /// The email of the employee. Leave empty string if the email should not be updated.
+        /// The email of the employee.
         /// </summary>
         [Required]
         public string Email { get; set; }
 
         /// <summary>
-        /// The first name of the employee. Leave empty string if the first name should not be updated.
+        /// The first name of the employee.
         /// </summary>
         [Required]
         public string FirstName { get; set; }
 
         /// <summary>
-        /// The last name of the employee. Leave empty string if the last name should not be updated.
+        /// The last name of the employee.
         /// </summary>
         [Required]
         public string LastName { get; set; }
 
         /// <summary>
-        /// The title id of the employee. Leave as 0 if the title should not be updated.
+        /// The title id of the employee.
         /// </summary>
         public int EmployeeTitleId { get; set; }
 
