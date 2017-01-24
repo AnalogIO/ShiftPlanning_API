a26cec8f8852e061d0626d375c25cb7730608032
xthedeus <fhov@itu.dk>
2017-01-24 15:54:39 +0100

    Fixed error if no Authorization header is provided

--- API/API/Authorization/AdminFilter.cs
+++ API/API/Authorization/AdminFilter.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
@@ -18,13 +19,15 @@ namespace API.Authorization
             var token = actionContext.Request.Headers.Authorization;
             if (token == null)
             {
-                actionContext.Response = actionContext.Request.CreateResponse<object>(HttpStatusCode.Unauthorized, new { Message = "Please set the 'Authorization' header to the token granted on login!" });
+                throw new UnauthorizedAccessException("Please set the 'Authorization' header to the token granted on login!");
+                //actionContext.Response = actionContext.Request.CreateResponse<object>(HttpStatusCode.Unauthorized, new { Message = "Please set the 'Authorization' header to the token granted on login!" });
             }
             else if (token != null)
             {
                 if (!TokenManager.ValidateLoginToken(token.ToString()))
                 {
-                    actionContext.Response = actionContext.Request.CreateResponse<object>(HttpStatusCode.Unauthorized, new { Message = "The provided token is not valid!" });
+                    throw new UnauthorizedAccessException("The provided token is not valid!");
+                    //actionContext.Response = actionContext.Request.CreateResponse<object>(HttpStatusCode.Unauthorized, new { Message = "The provided token is not valid!" });
                 }
             }
         }
