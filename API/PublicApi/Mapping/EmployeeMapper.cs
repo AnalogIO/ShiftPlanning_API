645d43310f8b46cc871806c2ed7587bbb8548335
xthedeus <fhov@itu.dk>
2017-04-05 21:34:34 +0200

    Fixed PhotoRef for employees

--- API/PublicApi/Mapping/EmployeeMapper.cs
+++ API/PublicApi/Mapping/EmployeeMapper.cs
@@ -15,12 +15,15 @@ namespace PublicApi.Mapping
 
             var portString = url.IsDefaultPort ? "" : $":{url.Port}";
 
+            var path = url.LocalPath;
+            var deployedPath = path.Substring(1, path.IndexOf("api") - 2);
+
             return employees.Select(emp => new EmployeeDTO
             {
                 FirstName = emp.FirstName,
                 LastName = emp.LastName,
                 Title = emp.EmployeeTitle.Title,
-                PhotoRef = $"{url.Scheme}://{url.Host}{portString}/{PhotosController.RoutePrefix}/{emp.Photo.Organization.ShortKey}/{emp.Photo.Id}"
+                PhotoRef = $"{url.Scheme}://{url.Host}{portString}/{deployedPath}/{PhotosController.RoutePrefix}/{emp.Photo.Organization.ShortKey}/{emp.Photo.Id}"
             });
         }
 
@@ -30,12 +33,15 @@ namespace PublicApi.Mapping
 
             var portString = url.IsDefaultPort ? "" : $":{url.Port}";
 
+            var path = url.LocalPath;
+            var deployedPath = path.Substring(1, path.IndexOf("api") - 2);
+
             return new EmployeeDTO
             {
                 FirstName = employee.FirstName,
                 LastName = employee.LastName,
                 Title = employee.EmployeeTitle.Title,
-                PhotoRef = $"{url.Scheme}://{url.Host}{portString}/{PhotosController.RoutePrefix}/{employee.Photo.Organization.ShortKey}/{employee.Photo.Id}"
+                PhotoRef = $"{url.Scheme}://{url.Host}{portString}/{deployedPath}/{PhotosController.RoutePrefix}/{employee.Photo.Organization.ShortKey}/{employee.Photo.Id}"
             };
         }
     }
