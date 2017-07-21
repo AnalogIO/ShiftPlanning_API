3213d329dc9680f75924ebd5ef1296c2d75cea88
xthedeus <fhov@itu.dk>
2017-07-21 17:08:48 +0200

    Added preferences of scheduledshifts to employees

--- API/Data/Models/Employee.cs
+++ API/Data/Models/Employee.cs
@@ -10,11 +10,12 @@ namespace Data.Models
         public string Salt { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public EmployeeTitle EmployeeTitle { get; set; }
+        public virtual EmployeeTitle EmployeeTitle { get; set; }
         public bool Active { get; set; }
         public virtual ICollection<Shift> Shifts { get; set; }
         public virtual ICollection<CheckIn> CheckIns { get; set; }
         public virtual ICollection<ScheduledShift> ScheduledShifts { get; set; }
+        public virtual ICollection<Preference> Preferences { get; set; }
         public virtual Organization Organization { get; set; }
         public virtual Photo Photo { get; set; }
         public virtual ICollection<Role> Roles { get; set; }
---
+++ API/Data/Models/Preference.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Preference
+    {
+        public int Id { get; set; }
+        public int Priority { get; set; }
+        public virtual Employee Employee { get; set; }
+        public virtual ScheduledShift ScheduledShift { get; set; }
+    }
+}
