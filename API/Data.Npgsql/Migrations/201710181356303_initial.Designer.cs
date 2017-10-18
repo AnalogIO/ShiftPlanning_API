b9cbbb89d69bea8c46c679aef29da760c38d216e
xthedeus <fhov@itu.dk>
2017-10-18 17:17:03 +0200

    migration fucked up when going to mssql. therefore regenerated initial migration.

--- API/Data.Npgsql/Migrations/201710181356303_initial.Designer.cs
+++ API/Data.Npgsql/Migrations/201710181356303_initial.Designer.cs
@@ -7,13 +7,13 @@ namespace Data.Npgsql.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class removedmodelbuilderstuff : IMigrationMetadata
+    public sealed partial class initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(removedmodelbuilderstuff));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201709040950382_removed modelbuilder stuff"; }
+            get { return "201710181356303_initial"; }
         }
         
         string IMigrationMetadata.Source
