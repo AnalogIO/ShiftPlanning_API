6caf475e002dbdbfbb8ffe35b5476017176892d5
xthedeus <fhov@itu.dk>
2018-05-23 14:52:17 +0200

    Requires json properties to be part of the request - always - empty if not to be updated as per agreement

--- API/DataTransferObjects/Shift/UpdateShiftDTO.cs
+++ API/DataTransferObjects/Shift/UpdateShiftDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace DataTransferObjects.Shift
 {
     public class UpdateShiftDTO
     {
+        [Required]
         public string Start { get; set; }
+        [Required]
         public string End { get; set; }
+        [Required]
         public int[] EmployeeIds { get; set; }
     }
 }
